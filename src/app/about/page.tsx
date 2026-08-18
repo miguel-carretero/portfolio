2108e0683f582bc581178deb4c753b71877d94a7
Miguel Carretero <starmac_forever00@hotmail.com>
2026-08-18 17:40:35 +0200

    feat : add portfolio page structure

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>À propos</h1>
+    </main>
+  );
+}
---
+++ src/app/trademind/page.tsx
@@ -0,0 +1,7 @@
+export default function TradeMindPage() {
+  return (
+    <main>
+      <h1>TradeMind</h1>
+    </main>
+  );
+}
